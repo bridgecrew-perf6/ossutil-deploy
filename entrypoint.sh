bb38ee169f344cf97a4085544b46b512f4920828
luker <luker@inc-acb.com>
2022-03-10 19:23:14 +0800

    [10001] publish

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh -l
 
-ossutil config -e $1 -i $4 -k access-key-secret: $5
+cat "[Credentials]
+        endpoint = $1
+        accessKeyID = $4
+        accessKeySecret = $5
+        " >> ~/.ossutilconfig
+# ossutil config -e $1 -i $4 -k access-key-secret: $5
 ossutil cp -rfu $2 $3
