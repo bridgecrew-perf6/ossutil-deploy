0c8b1fac23db6d3fa5de13ddf2a61158d417a0b6
luker <luker@inc-acb.com>
2022-03-10 19:34:21 +0800

    feat: change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
-touch ~/.ossutilconfig
+touch /root/ossutilconfig
 echo "[Credentials]
         endpoint = $1
         accessKeyID = $4
         accessKeySecret = $5
-        " >> ~/.ossutilconfig
+        " >> /root/ossutilconfig
 # ossutil config -e $1 -i $4 -k access-key-secret: $5
 ossutil cp -rfu $2 $3
